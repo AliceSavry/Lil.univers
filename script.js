8065960d8141054a348a2c7711c0701166140a0b
SAVRY Alice <165331699+AliceSavry@users.noreply.github.com>
2024-12-03 08:52:07 +0100

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function closeLightbox() {
     lightboxImage.src = '';
 }
 
-// Empêche le clic droit sur les images
+// Empêcher le clic droit sur les images
 document.addEventListener('contextmenu', function (event) {
     if (event.target.tagName === 'IMG') {
         event.preventDefault();
